25626c15b0db70b9d75a744ac37fa861a975e4e9
Ruosong Wang <72921237+bitGrayFalcon@users.noreply.github.com>
2024-11-21 09:54:35 +0800

    Update muti-rtsp.cpp

--- multi-rtsp-master/src/muti-rtsp.cpp
+++ multi-rtsp-master/src/muti-rtsp.cpp
@@ -10,7 +10,6 @@
 
 class RtspCamera {
 public:
-    // 属性
     std::string rtsp_address;
     std::string ros_image_topic;
     image_transport::Publisher image_publisher;
@@ -18,7 +17,6 @@ public:
     std::mutex running_mutex;
     bool running;
 
-    // 构造函数
     RtspCamera(const std::string& address, const std::string& topic, ros::NodeHandle& nh)
         : rtsp_address(address), ros_image_topic(topic), running(true) {
         // 初始化发布者
@@ -29,7 +27,6 @@ public:
         capture_thread = std::thread(&RtspCamera::captureAndPublish, this);
     }
 
-    // 析构函数
     ~RtspCamera() {
         {
             std::lock_guard<std::mutex> lock(running_mutex);
@@ -40,7 +37,7 @@ public:
         }
     }
 
-    // 捕获和发布图像的方法
+    //捕获和发布图像
     void captureAndPublish() {
         cv::VideoCapture cap(rtsp_address);
         if (!cap.isOpened()) {
@@ -76,14 +73,13 @@ public:
 };
 
 int main(int argc, char **argv) {
-    // 初始化ROS节点
     ros::init(argc, argv, "rtsp_camera_node");
     ros::NodeHandle nh;
 
-    // 初始化RtspCamera对象
-    RtspCamera right("rtsp://admin:cssrc@702@192.168.144.64:554/Streaming/Channels/101", "/camera/right", nh);
-    RtspCamera mid("rtsp://admin:cssrc@702@192.168.144.65:554/Streaming/Channels/101", "/camera/mid", nh);
-    RtspCamera left("rtsp://admin:cssrc@702@192.168.144.66:554/Streaming/Channels/101", "/camera/left", nh);
+    // 初始化RtspCamera对象,可以初始化复数个对象
+    RtspCamera right("rtsp://192.168.100.64:8000/Streaming/Channels/101", "/camera/right", nh);
+    RtspCamera mid("rtsp://admin:123456@192.168.100.65:8000/Streaming/Channels/101", "/camera/mid", nh);
+    RtspCamera left("rtsp://user:pwd@192.168.100.66:8000/Streaming/Channels/101", "/camera/left", nh);
 
     // 保持节点运行
     ros::spin();
